7873b848107b0d4ef1c49b6eebaa9070540a7300
Garry3Fingers <www.vlad78@yandex.ru>
2022-10-04 22:09:34 +0300

    Create a form for input

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,42 @@
-const containerGrid = document.querySelector('.container-grid');
+const gridContainer = document.querySelector('.grid-container');
 
 for (let i = 0; i < 256; i++) {
   let div = document.createElement('div');
-  containerGrid.appendChild(div);
+  gridContainer.appendChild(div);
 };
 
-const gridDiv = document.querySelectorAll('.container-grid div');
+const gridDiv = document.querySelectorAll('.grid-container div');
 
 gridDiv.forEach((div) => {
   div.addEventListener('mouseenter', () => {
-    div.className = 'flex-div'
+    div.className = 'grid-div';
   })
 });
+
+const formContainer = document.querySelector('.form-container form');
+
+formContainer.addEventListener('submit', getGridSize);
+
+function getGridSize(e) {
+  
+  const horizontally = +document.getElementById('horizontally').value;
+
+  const vertically = +document.getElementById('vertically').value;
+
+  let gridSize = horizontally * vertically;
+
+  const gridSizeValues = [horizontally, vertically, gridSize];
+
+  e.preventDefault();
+
+  return gridSizeValues;
+
+};
+
+// formContainer.addEventListener('submit', createGrid);
+
+// function createGrid() { 
+  
+  
+
+// };
