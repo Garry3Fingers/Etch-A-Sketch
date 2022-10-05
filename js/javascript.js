91853cde986c1ea58d66d89914a35717203cecb2
Garry3Fingers <www.vlad78@yandex.ru>
2022-10-05 12:05:27 +0300

    Implemented creation of grid

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,45 @@
 const gridContainer = document.querySelector('.grid-container');
 
-for (let i = 0; i < 256; i++) {
-  let div = document.createElement('div');
-  gridContainer.appendChild(div);
+// for (let i = 0; i < 256; i++) {
+//   let div = document.createElement('div');
+//   gridContainer.appendChild(div);
+// };
+
+
+
+const formContainer = document.querySelector('.form-container form');
+
+function getGridSize() {
+  
+  const horizontalSquares = +document.getElementById('horizontally').value;
+
+  const verticalSquares = +document.getElementById('vertically').value;
+
+  let gridOfSquare = horizontalSquares * verticalSquares;
+
+  const gridSizeValues = [horizontalSquares, verticalSquares, gridOfSquare];
+
+  return gridSizeValues;
+
+};
+
+formContainer.addEventListener('submit', createGrid);
+
+function createGrid(e) { 
+  
+  const [horizontal, vertical, numberSquares] = getGridSize();
+
+  gridContainer.style.height = `${vertical * 25}px`;
+
+  gridContainer.style.width = `${horizontal * 25}px`;
+
+  for (let i = 0; i < numberSquares; i++) {
+    let div = document.createElement('div');
+    gridContainer.appendChild(div);
+  };
+  
+  e.preventDefault();
+
 };
 
 const gridDiv = document.querySelectorAll('.grid-container div');
@@ -12,31 +49,3 @@ gridDiv.forEach((div) => {
     div.className = 'grid-div';
   })
 });
-
-const formContainer = document.querySelector('.form-container form');
-
-formContainer.addEventListener('submit', getGridSize);
-
-function getGridSize(e) {
-  
-  const horizontally = +document.getElementById('horizontally').value;
-
-  const vertically = +document.getElementById('vertically').value;
-
-  let gridSize = horizontally * vertically;
-
-  const gridSizeValues = [horizontally, vertically, gridSize];
-
-  e.preventDefault();
-
-  return gridSizeValues;
-
-};
-
-// formContainer.addEventListener('submit', createGrid);
-
-// function createGrid() { 
-  
-  
-
-// };
